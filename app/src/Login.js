c3d75bb931aee8eb5ca7969a1a3bb022f7e50eee
Artur Fracala <arturfracala@gmail.com>
2019-06-17 10:58:15 +0200

    correct login page

--- app/src/Login.js
+++ app/src/Login.js
@@ -1,35 +1,28 @@
 import React, {Component} from 'react';
-import {Button, FormGroup, FormControl} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 
 export default class Login extends Component{
-    constructor(props) {
+    constructor(props){
         super(props);
-
-        this.state = {
-            data: [],
-            nickname: "",
-            password: ""
-        };
-
-        this.validateForm = this.validateForm.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = { username: '', password: '', data:[] };
     }
+
+    handleChange = ({ target }) => {
+        this.setState({ [target.name]: target.value });
+    };
+
 
     validateForm() {
-        return this.state.nickname.length > 0 && this.state.password.length > 0;
+        return this.state.username.length > 0 && this.state.password.length > 0;
     }
 
-    handleChange = event => {
-        this.setState({
-            [event.target.id]: event.target.value
-        });
-    }
 
-    handleSubmit = (event, {history}) => {
-        this.state.data.map( function (user ) {
-            if (user.nick === this.state.nickname && user.password === this.state.password)
-                history.push('/student');
+    handleSubmit = (event) => {
+        this.state.data.map( (user) => {
+            if (user.email === this.state.username && user.password === this.state.password)
+            {
+               this.props.history.push("/student");
+            }
         });
     }
 
@@ -44,35 +37,28 @@ export default class Login extends Component{
 
     render() {
         return (
-            <div className="Login">
-                <form onSubmit={this.handleSubmit}>
-                    <FormGroup controlId="text">
-                        <p>Username</p>
-                        <FormControl
-                            autoFocus
-                            type="text"
-                            value={this.state.nickname}
-                            onChange={this.handleChange}
-                        />
-                    </FormGroup>
-                    <FormGroup controlId="password">
-                        <p>Password</p>
-                        <FormControl
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            type="password"
-                        />
-                    </FormGroup>
-                    <Button
-                        block
-                        bsSize="large"
-                        disabled={!this.validateForm()}
-                        type="submit"
-                    >
-                        Login
-                    </Button>
+            <React.Fragment>
+                <h3>Enter your data</h3>
+                <form>
+                    <label htmlFor="username">Email:</label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
                 </form>
-            </div>
+
+                <Button type="submit" disabled={!this.validateForm()} onClick={this.handleSubmit}>Login</Button>
+            </React.Fragment>
         );
     }
 }
